0ffb4c4aa50f050728f64612bd03b29504493baa
Douglas Garcia <douglasgovmad@gmail.com>
2022-08-06 19:53:04 -0300

    commmit 4

---
+++ lista5-FACquest4.c
@@ -0,0 +1,25 @@
+/*
+
+	QUESTÃO 04:
+	Implementar um programa que exiba os N
+	primeiros termos de uma PA (Progressão
+	Aritmética) com primeiro termo a1 e razão
+	r.
+
+*/
+#include <stdio.h>
+
+void main(){
+	int i,a1,n,r;
+	
+	printf("\nEnter a1:\n");
+	scanf("%d",&a1);
+	printf("\nEnter n:\n");
+	scanf("%d",&n);
+	printf("\nEnter r:\n");
+	scanf("%d",&r);
+	
+	for(i=a1;i<=n;i=i+r){
+		printf(" %d",i);
+	}
+}
